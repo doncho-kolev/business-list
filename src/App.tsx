d8457803d4c68b74c744ae9c78e41fadde2a9320
Doncho Kolev <doncho.kolev@concordeu.com>
2022-07-14 11:21:00 +0300

    Create simple components and routes for main views

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+} from "react-router-dom";
+import List from './components/list/List';
+import Business from './components/business/Business';
+import NotFound from './components/notFound/NotFound';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    return (
+        <Router>
+            <Routes>
+                <Route index element={<List />} />
+                <Route path="/business/:id" element={<Business />} />
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+        </Router>
+    );
 }
-
-export default App;
